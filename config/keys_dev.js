1ec8a7bdce428bce2b288d2e43fd928b44e49d3e
mwomack <michael.womack84@gmail.com>
2019-03-27 19:44:36 -0700

    Heroku deploy

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://mike12345:mike12345@cluster0-soo3n.mongodb.net/test?retryWrites=true"
+};
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const todos = require("./routes/todos");
 
 const app = express();
@@ -18,6 +18,16 @@ mongoose
 
 // Use routes
 app.use("/api/todos", todos);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
